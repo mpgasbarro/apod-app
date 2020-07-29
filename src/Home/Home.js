caf63b277bc00f1fc61833e6944a4f92a37e378b
Gaspy <mpgasbarro@gmail.com>
2020-07-29 13:14:18 -0400

    populated the props on the Homepage. Everything is coming through. Limited to no styling done at this point

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,10 +6,15 @@ class Home extends Component {
     render() {
         console.log(this.props.apod);
         return (
-            <div>
-                
-            </div>
-        );
+					<div>
+                        <h2 className="apodtitle"> {this.props.apod.title} </h2>
+                        <h5 className="copyright"> Copyright: {this.props.apod.copyright} </h5>
+                        <div> 
+						<img className="pictureOfDay" src={this.props.apod.url} alt={this.props.apod.explanation} />
+                        <p className="apodDescription"> {this.props.apod.explanation} </p>
+                        </div>
+					</div>
+				);
     }
 }
 
