fd85ddc2f2a2041b7f696dfdb049ef521a0462c1
Gaspy <mpgasbarro@gmail.com>
2020-08-03 09:33:03 -0400

    added styling to buttons, including calendar as well

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import "./Calendar.css"
 
 
 function Calendar(props) {
 	return (
 		<div>
-			<DatePicker
+			<DatePicker className="calendar"
 				selected={props.selectedDate}
 				onChange={(date) => props.handleDateSelection(date)}
 				maxDate={new Date()}
 				isClearable
-				placeholderText='Select Picture by Date'
+                placeholderText='Select by Date'
+                
 			/>
 		</div>
 	);
